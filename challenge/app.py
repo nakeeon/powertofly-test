de2a60fd57575a07947c62b80c3085c8fa26ed4e
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-22 19:01:02 +0200

    Set up redis

--- challenge/app.py
+++ challenge/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_caching import Cache
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,3 +9,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 api = Api(app)
 db = SQLAlchemy(app)
+
+cache = Cache(config={
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_REDIS_HOST': os.environ.get('REDIS_HOST'),
+    'CACHE_REDIS_PORT': os.environ.get('REDIS_PORT'),
+})
+cache.init_app(app)
