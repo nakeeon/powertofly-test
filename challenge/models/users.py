f3136cbea8d25e43ce74725da99791184b5ba660
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-22 18:40:00 +0200

    Create User model

---
+++ challenge/models/users.py
@@ -0,0 +1,14 @@
+from challenge.app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __str__(self):
+        return f'<User {self.username}>'
