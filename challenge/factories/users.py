bbee1dc6271b2717d005d3691600a181fe5e0640
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-22 20:57:39 +0200

    Create UserFactory

---
+++ challenge/factories/users.py
@@ -0,0 +1,15 @@
+import factory
+from challenge.app import db
+from challenge.models.users import User
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = User
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = 'commit'
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
--- challenge/models/users.py
+++ challenge/models/users.py
@@ -7,8 +7,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
     def __str__(self):
         return f'<User {self.username}>'
