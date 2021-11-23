b0f67159f3d81215ae384dcba6e856598084e877
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-23 12:13:31 +0200

    Use CACHE_REDIS_URL to connect to redis

--- challenge/common.py
+++ challenge/common.py
@@ -4,6 +4,5 @@ from flask_caching import Cache
 
 cache = Cache(config={
     'CACHE_TYPE': 'RedisCache',
-    'CACHE_REDIS_HOST': os.environ.get('REDIS_HOST'),
-    'CACHE_REDIS_PORT': os.environ.get('REDIS_PORT'),
+    'CACHE_REDIS_URL': os.environ.get('CACHE_REDIS_URL')
 })
