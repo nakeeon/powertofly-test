80bf94a375933de3af3d13d6e3185f9a60ff517e
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-23 15:19:47 +0200

    Update redis connection

--- challenge/common.py
+++ challenge/common.py
@@ -1,8 +1,15 @@
 import os
+from urllib.parse import urlparse
 
+import redis
 from flask_caching import Cache
+
+url = urlparse(os.environ.get('REDIS_URL'))
+r = redis.Redis(host=url.hostname, port=url.port, username=url.username,
+                password=url.password, ssl=os.environ.get('REDIS_SSL', False),
+                ssl_cert_reqs=None)
 
 cache = Cache(config={
     'CACHE_TYPE': 'RedisCache',
-    'CACHE_REDIS_URL': os.environ.get('REDIS_URL')
+    'CACHE_REDIS_HOST': r,
 })
