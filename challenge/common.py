659152f54b76b56ed592a2b7325409856f126a20
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-11-23 15:33:03 +0200

    Use REDIS_TLS_URL env variable

--- challenge/common.py
+++ challenge/common.py
@@ -4,10 +4,15 @@ from urllib.parse import urlparse
 import redis
 from flask_caching import Cache
 
-url = urlparse(os.environ.get('REDIS_URL'))
-r = redis.Redis(host=url.hostname, port=url.port, username=url.username,
-                password=url.password, ssl=os.environ.get('REDIS_SSL', False),
-                ssl_cert_reqs=None)
+url = urlparse(os.environ.get('REDIS_TLS_URL'))
+r = redis.Redis(
+    host=url.hostname,
+    port=url.port,
+    username=url.username,
+    password=url.password,
+    ssl=os.environ.get('REDIS_SSL', False),
+    ssl_cert_reqs=None
+)
 
 cache = Cache(config={
     'CACHE_TYPE': 'RedisCache',
